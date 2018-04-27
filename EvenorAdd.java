a811f323142547f6429c54783961fad1f5742e99
Niroshan-Selvaraj <38588745+Niroshan-Selvaraj@users.noreply.github.com>
2018-04-27 07:21:07 +0530

    Create EvenorAdd.java

---
+++ EvenorAdd.java
@@ -0,0 +1,13 @@
+public class EvenorAdd{
+public static void main(String args[]){
+Scanner input = new Scanner(System.in);
+int number = input.nextInt();
+if(number%2==0){
+System.out.println("Even");
+System.out.println("Odd");
+else
+
+
+}
+
+}
