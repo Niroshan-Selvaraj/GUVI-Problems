52f10780afb04af197efe2fc53131f930b99096a
Niroshan-Selvaraj <38588745+Niroshan-Selvaraj@users.noreply.github.com>
2018-04-27 07:23:46 +0530

    Update EvenorAdd.java

--- EvenorAdd.java
+++ EvenorAdd.java
@@ -2,12 +2,16 @@ public class EvenorAdd{
 public static void main(String args[]){
 Scanner input = new Scanner(System.in);
 int number = input.nextInt();
+  
+  
+  if(1<=number<=100000){
 if(number%2==0){
 System.out.println("Even");
 else
 System.out.println("Odd");
-
-
 }
+  }else{
+  System.out.println("Invalid input");
+  }
 
 }
