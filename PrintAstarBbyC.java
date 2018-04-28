76cdefd48318847b2f44e95363e75ac9eb56673b
Niroshan-Selvaraj <38588745+Niroshan-Selvaraj@users.noreply.github.com>
2018-04-28 21:00:59 +0530

    Create PrintAstarBbyC.java

---
+++ PrintAstarBbyC.java
@@ -0,0 +1,10 @@
+public class PrintAstarBbyC{
+public static void main(String args[]){
+Scanner input = new Scanner(System.in);
+		int A = input.nextInt();
+		int B = input.nextInt();
+		int C = input.nextInt();
+		int result = (A * B) / C;
+		System.out.print(result);
+    }
+}
