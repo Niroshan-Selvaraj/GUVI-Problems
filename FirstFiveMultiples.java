ff2e63d402ef5ecaac428d2e04766140e0d3c1ab
Niroshan-Selvaraj <38588745+Niroshan-Selvaraj@users.noreply.github.com>
2018-04-28 17:51:29 +0530

    Create FirstFiveMultiples.java

---
+++ FirstFiveMultiples.java
@@ -0,0 +1,10 @@
+public class FirstFiveMultiple{
+public static void main(String args[]){
+Scanner scan = new Scanner(System.in);
+	int number = scan.nextInt();
+	for(int i=1;i<=5;i++){
+		System.out.print((number*i)+" ");
+		}
+}
+
+}
