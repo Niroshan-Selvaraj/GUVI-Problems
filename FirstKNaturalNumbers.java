090a3654131007177b0cb5083c66af15bc2a9c06
Niroshan-Selvaraj <38588745+Niroshan-Selvaraj@users.noreply.github.com>
2018-04-28 21:30:44 +0530

    Update FirstKNaturalNumbers.java

--- FirstKNaturalNumbers.java
+++ FirstKNaturalNumbers.java
@@ -1,23 +1,10 @@
 public class FirstKNaturalNumbers{
 public static void main(String args[]){
-Scanner input = new Scanner(System.in);
-		int temp = 0;
-		int arr[] = new int[10];
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = input.nextInt();
 
-		}
-		// Sorting in Ascending order
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = i + 1; j < arr.length; j++) {
-				if (arr[i] > arr[j]) {
-					temp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp;
-				}
-			}
-		}
-		System.out.print(arr[0]);
+		Scanner input = new Scanner(System.in);
+		int number = input.nextInt();
+		int result = number * (number + 1) / 2;
+		System.out.print(result);
 
 
 }
