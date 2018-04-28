9176c3a8fdf3974e50c9047e47cb07cd57ee36f2
Niroshan-Selvaraj <38588745+Niroshan-Selvaraj@users.noreply.github.com>
2018-04-28 11:25:02 +0530

    Update CheckAlpha.java

--- CheckAlpha.java
+++ CheckAlpha.java
@@ -3,9 +3,9 @@ public static void main(String args[]){
 Scanner input = new Scanner(System.in);
 char ch = input.nextLine().charAt(0);
 if(ch>='a' && ch<='z'||ch>='A' && ch<='Z')
-	System.out.println("The entered character is an Aplhabet");
+	System.out.println("Aplhabet");
 else
-	System.out.println("The entered character is not an Alphabet character");
+	System.out.println("No");
 
 
 }
