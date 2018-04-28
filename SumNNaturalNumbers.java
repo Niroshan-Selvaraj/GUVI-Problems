7dd3f3e3870d35092e848217fe79dd63fa5e93b9
Niroshan-Selvaraj <38588745+Niroshan-Selvaraj@users.noreply.github.com>
2018-04-28 13:14:27 +0530

    Create SumNNaturalNumbers.java

---
+++ SumNNaturalNumbers.java
@@ -0,0 +1,13 @@
+public class SumNNaturalNumbers{
+
+public static void main(String args[]){
+Scanner input = new Scanner(System.in);
+int n = input.nextInt();
+int sum = 0;
+for(int i=0;i<n;i++){
+sum += i+1;
+}
+System.out.println(sum);
+}
+
+}
