bc32bc4c16eecf3bd01b1e25d74b883d08530793
Niroshan-Selvaraj <38588745+Niroshan-Selvaraj@users.noreply.github.com>
2018-04-28 11:25:23 +0530

    Update CheckAlpha.java

--- CheckAlpha.java
+++ CheckAlpha.java
@@ -3,7 +3,7 @@ public static void main(String args[]){
 Scanner input = new Scanner(System.in);
 char ch = input.nextLine().charAt(0);
 if(ch>='a' && ch<='z'||ch>='A' && ch<='Z')
-	System.out.println("Aplhabet");
+	System.out.println("Alphbet");
 else
 	System.out.println("No");
 
