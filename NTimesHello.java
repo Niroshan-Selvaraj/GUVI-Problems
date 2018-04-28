d415df555f6678ab435128e1d125082ec7309d2d
Niroshan-Selvaraj <38588745+Niroshan-Selvaraj@users.noreply.github.com>
2018-04-28 12:59:41 +0530

    Create NTimesHello.java

---
+++ NTimesHello.java
@@ -0,0 +1,13 @@
+public class NTimesHello{
+
+public static void main(String args[]){
+Scanner scan = new Scanner(System.in);
+int input = scan.nextInt();
+
+for(int i=0;i<input;i++){
+System.out.println("Hello");
+}
+
+}
+
+}
