a3406c9b5554b2c691c7805645897637ce7ceca7
Niroshan-Selvaraj <38588745+Niroshan-Selvaraj@users.noreply.github.com>
2018-04-28 19:43:30 +0530

    Create NKJoinIT.java

---
+++ NKJoinIT.java
@@ -0,0 +1,11 @@
+public class NKJoinIT{
+public static void main(String args[]){
+Scanner input = new Scanner(System.in);
+String str1 = input.nextLine();
+String str2 = input.nextLine();
+System.out.println(str1+str2);
+
+
+}
+
+}
