db242832fef48526bf5098c466e4b4de8b5e46de
Niroshan-Selvaraj <38588745+Niroshan-Selvaraj@users.noreply.github.com>
2018-05-06 22:21:28 +0530

    Create CodekataHunter2.java

---
+++ CodekataHunter2.java
@@ -0,0 +1,29 @@
+public class CodekataHunter2 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		 StringBuilder result = new StringBuilder();
+
+		int arr[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			
+			arr[i] = input.nextInt();
+		}
+		int temp = 0;
+
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] < arr[j]) {
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+for(int i=0;i<arr.length;i++){
+	result.append(arr[i]);
+	
+}
+System.out.println(result.toString());
+	}
+}
